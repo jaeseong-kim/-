3566b03476bae43d3ea0187f8d0c4d709d881332
jaeseong-kim <61345774+jaeseong-kim@users.noreply.github.com>
2023-03-15 18:50:38 +0900

    [level 1] Title: 폰켓몬, Time: 1.97 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -3,19 +3,19 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        
-        int num = nums.length/2;
         Set<Integer> set = new HashSet();
+        int num = nums.length/2;
         
+        //총 종류의 개수 구하기
         for(int i : nums){
             if(!set.contains(i)){
                 set.add(i);
             }
         }
         
-        answer = num < set.size() ? num : set.size();
-        
-        return answer;
+        //종류의 개수 구하기
+        //종류의 개수가 내가 가져갈 수 보다 많으면 -> 내가 가져갈 수가 답
+        //종류의 개수보다 내가 가져갈 수가 많으면 -> 종류의 개수가 답
+        return num < set.size() ? num : set.size();
     }
 }
