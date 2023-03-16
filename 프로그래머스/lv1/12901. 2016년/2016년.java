2a6b31a045dfbf12b67ac72bbf2f5b89c9a59d84
jaeseong-kim <61345774+jaeseong-kim@users.noreply.github.com>
2023-03-16 15:15:24 +0900

    [level 1] Title: 2016년, Time: 1.23 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+
+
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        
+        LocalDate date = LocalDate.of(2016,a,b);
+        
+        switch(date.getDayOfWeek().getValue()){
+            case 1:
+                answer = "MON";
+                break;
+            case 2:
+                answer = "TUE";
+                break;
+            case 3:
+                answer = "WED";
+                break;
+            case 4:
+                answer = "THU";
+                break;
+            case 5:
+                answer = "FRI";
+                break;
+            case 6:
+                answer = "SAT";
+                break;
+            case 7:
+                answer = "SUN";
+                break;
+        }
+        
+        System.out.println(answer);
+        
+        return answer;
+    }
+}
