d804a289863318d7ada6b3b12495e3cc05a8aa47
jaeseong-kim <61345774+jaeseong-kim@users.noreply.github.com>
2023-03-28 17:05:32 +0900

    [Bronze I] Title: 이항 계수 1, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n+1][n+1];
+        for(int i = 0; i < arr.length; i++){
+            arr[i][0] = 1;
+            arr[i][1] = i;
+            arr[i][i] = 1;
+        }
+
+        for(int i = 2; i < arr.length; i++){
+            for(int j = 1; j<i; j++){
+                arr[i][j] = arr[i-1][j-1]+arr[i-1][j];
+            }
+        }
+
+        System.out.println(arr[n][k]);
+    }
+}
