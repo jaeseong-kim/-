0681d9e5dff377468431353eca22b7e410d7b74a
jaeseong-kim <61345774+jaeseong-kim@users.noreply.github.com>
2023-04-06 15:22:53 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,7 +1,6 @@
 import java.io.*;
 
 public class Main {
-    public static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,38 +8,28 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
         StringBuffer sb = new StringBuffer();
-        answer = 0;
-
+        int[] dp = new int[11];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        
         // 경우의 수 구하기
-        while (T > 0) {
-
-            int N = Integer.parseInt(br.readLine());
-
-            dfs(N);
-            
-            sb.append(answer).append("\n");
-
-            answer = 0;
-            T--;
-        }
+       while(T > 0){
+           int N = Integer.parseInt(br.readLine());
+           
+           if(N>3){
+               for(int i = 4; i <= N; i++){
+                   dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+               }
+           }
+           
+           sb.append(dp[N]).append("\n");
+           T--;
+       }
 
         // 출력
         bw.write(sb.toString());
         bw.close();
-    }
-
-    public static void dfs(int n) {
-        if (n < 0) {
-            return;
-        }
-
-        if (n == 0) {
-            answer++;
-            return;
-        }
-
-        for (int i = 1; i < 4; i++) {
-            dfs(n - i);
-        }
     }
 }
