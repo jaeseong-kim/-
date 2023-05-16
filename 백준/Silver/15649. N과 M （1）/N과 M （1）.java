7dc0f74123621c4c2e60e0d2f6d18fca7742d86b
jaeseong-kim <61345774+jaeseong-kim@users.noreply.github.com>
2023-05-16 16:00:17 +0900

    [Silver III] Title: N과 M (1), Time: 2188 ms, Memory: 69296 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static Integer[] arr;
+    public static boolean[] visited;
+
+    public static ArrayList<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // N, M 입력
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        // 자연수 배열 입력
+        arr = new Integer[N];
+        visited = new boolean[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        
+        // 백트래킹
+        list = new ArrayList<>();
+        combination(0, M);
+
+    }
+
+    public static void combination(int cur, int depth) {
+        if (cur == depth) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i) + " ");
+            }
+
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(arr[i]);
+                combination(cur + 1, depth);
+                list.remove(arr[i]);
+                visited[i] = false;
+            }
+        }
+    }
+}
