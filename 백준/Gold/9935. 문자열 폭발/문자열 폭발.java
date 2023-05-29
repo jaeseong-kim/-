c0e96efa6ca02ba148eee91f36123cef9468d7d7
jaeseong-kim <doctorwho123@naver.com>
2023-05-29 14:22:34 +0900

    [Gold IV] Title: 문자열 폭발, Time: 376 ms, Memory: 23408 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,3 +1,9 @@
+/*
+    String은 값이 바뀔 때마다 새로운 객체를 생성하기 때문에 메모리 초과가 발생한다.
+    StringBuffer, StringBuilder에 처음부터 문자열 값을 다 넣어놓고 문자열 폭탄을 제거하는 방식으로 풀면 시간초과가 난다.
+    remove()나 substring()을 하면 시간이 오래 걸리기 때문이다.
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,6 +15,8 @@ public class Main {
         String str = br.readLine();
         String bomb = br.readLine();
         StringBuilder sb = new StringBuilder();
+        
+        int bombLength = bomb.length();
 
         for (int i = 0; i < str.length(); i++) {
 
@@ -16,10 +24,10 @@ public class Main {
             sb.append(str.charAt(i));
 
             // 폭탄 문자열보다 길어지면 sb 검사
-            if (sb.length() >= bomb.length()) {
+            if (sb.length() >= bombLength) {
                 boolean sameFlag = true;
-                for (int j = 0; j < bomb.length(); j++) {
-                    char c1 = sb.charAt(sb.length() - bomb.length() + j);
+                for (int j = 0; j < bombLength; j++) {
+                    char c1 = sb.charAt(sb.length() - bombLength + j);
                     char c2 = bomb.charAt(j);
                     if (c1 != c2) {
                         sameFlag = false;
@@ -28,7 +36,7 @@ public class Main {
                 }
 
                 if (sameFlag) {
-                    sb.delete(sb.length() - bomb.length(), sb.length());
+                    sb.delete(sb.length() - bombLength, sb.length());
                 }
             }
         }
