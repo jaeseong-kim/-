bb2754e206acc8f05605b11aa365bffc464cc369
jaeseong-kim <doctorwho123@naver.com>
2023-06-22 16:35:35 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 256 ms, Memory: 19052 KB -BaekjoonHub

--- 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
@@ -5,15 +5,13 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
+        // 문자열 입력
         String input = br.readLine();
-
+        
+        // 펠린드롬이 아닌 최장 문자열 길이 구하기
         int answer = -1;
-        if (!input.matches(
-                "A+||B+||C+||D+||E+||F+||G+" +
-                        "||H+||I+||J+||K+||L+||M+||N+" +
-                        "||O+||P+||Q+||R+||S+||T+||U+" +
-                        "||V+||W+||X+||Y+||Z+")) {
+        if (!isAllSame(input)) {
             for (int i = input.length(); i > -1; i--) {
                 String subString = input.substring(0, i);
 
@@ -24,7 +22,8 @@ public class Main {
 
             }
         }
-
+        
+        // 출력
         System.out.println(answer);
     }
 
@@ -46,5 +45,13 @@ public class Main {
         }
 
         return true;
+    }
+
+    public static boolean isAllSame(String input) {
+        return input.matches(
+                "A+||B+||C+||D+||E+||F+||G+" +
+                        "||H+||I+||J+||K+||L+||M+||N+" +
+                        "||O+||P+||Q+||R+||S+||T+||U+" +
+                        "||V+||W+||X+||Y+||Z+");
     }
 }
