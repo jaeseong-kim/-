d55a23a9373cec26db1cadddf24408a664d2699e
jaeseong-kim <doctorwho123@naver.com>
2024-07-06 02:29:51 +0900

    [Bronze IV] Title: 영수증, Time: 112 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.kt
+++ 백준/Bronze/25304. 영수증/영수증.kt
@@ -1,24 +1,27 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
-	
-    val X = br.readLine().toInt()
     
+    val X = br.readLine().toInt()
     val N = br.readLine().toInt()
     
-    var amount = 0
-    for(i in 1.. N){
+    var sum = 0
+    for(i in 1..N){
         var item = br.readLine().split(" ")
-        amount += item[0].toInt()*item[1].toInt()
+        
+        val price =  item[0].toInt()
+        val amount = item[1].toInt()
+        
+        sum+=price*amount
     }
     
-    if(amount == X){
-        print("Yes")
+    var answer = ""
+    if(X == sum){
+        answer = "Yes"
     }else{
-        print("No")
+        answer = "No"
     }
-    
+    print(answer)
 }
